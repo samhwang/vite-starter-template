d6f75b963a35aafde652ef452c8c5130907ed276
Sam Huynh <samhwang2112.dev@gmail.com>
2022-07-18 11:31:32 +1000

    Refactor sample App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-it('renders learn React link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Learn React/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  it('renders learn React link and the state number button', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Learn React/i);
+    expect(linkElement).toBeInTheDocument();
+
+    const buttonElement = screen.getByRole('button', {
+      name: /count is: 0/i,
+    });
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  it('increase the state number button when click', () => {
+    render(<App />);
+    const buttonElement = screen.getByRole('button', {
+      name: /count is: 0/i,
+    });
+    fireEvent.click(buttonElement);
+
+    const countText = screen.getByText(/count is: 1/i);
+    expect(countText).toBeInTheDocument();
+  });
 });
