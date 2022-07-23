a10cb33539a88bdffa50655318c8e02ca7b1d349
Sam Huynh <samhwang2112.dev@gmail.com>
2022-07-23 15:26:47 +1000

    Mkae typescript be able to read vitest types and extend matcher

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,18 @@
-import '@testing-library/jest-dom';
-import { beforeAll, afterAll, afterEach } from 'vitest';
+import matchers, {
+  TestingLibraryMatchers,
+} from '@testing-library/jest-dom/matchers';
+import { beforeAll, afterAll, afterEach, expect } from 'vitest';
 import { server } from './__mocks__/msw/server';
+
+declare global {
+  namespace Vi {
+    interface JestAssertion<T = any>
+      extends jest.Matchers<void, T>,
+        TestingLibraryMatchers<T, void> {}
+  }
+}
+
+expect.extend(matchers);
 
 beforeAll(() => server.listen());
 
