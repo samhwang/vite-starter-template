9a8f729ede47af9d0960583e2dafc3baf11bb989
Sam Huynh <samhwang2112.dev@gmail.com>
2022-08-22 16:45:58 +1000

    Update to the latest vite react template

--- README.md
+++ README.md
@@ -22,7 +22,6 @@ In the project directory, you can run:
 ### `npm run dev`
 
 Runs the app in the development mode.
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 
 The page will reload when you make changes.
 You may also see any lint errors in the console.
--- src/app.test.tsx
+++ src/app.test.tsx
@@ -3,25 +3,14 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import App from './app';
 
 describe('App component', () => {
-  it('renders learn React link and the state number button', () => {
-    render(<App />);
-    const linkElement = screen.getByText(/Learn React/i);
-    expect(linkElement).toBeInTheDocument();
-
-    const buttonElement = screen.getByRole('button', {
-      name: /count is: 0/i,
-    });
-    expect(buttonElement).toBeInTheDocument();
-  });
-
   it('increase the state number button when click', () => {
     render(<App />);
     const buttonElement = screen.getByRole('button', {
-      name: /count is: 0/i,
+      name: /count is 0/i,
     });
     fireEvent.click(buttonElement);
 
-    const countText = screen.getByText(/count is: 1/i);
+    const countText = screen.getByText(/count is 1/i);
     expect(countText).toBeInTheDocument();
   });
 });
