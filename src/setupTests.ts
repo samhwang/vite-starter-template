3c3ca97e6bd2a8ecec33ef455f9448bb6bbc7b7e
Sam Huynh <samhwang2112.dev@gmail.com>
2023-08-08 18:55:15 +1000

    chore: remove manual overriden of vitest jest expect types

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,13 +1,7 @@
-import matchers, { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers';
+import matchers from '@testing-library/jest-dom/matchers';
 import { cleanup } from '@testing-library/react';
 import { beforeAll, afterAll, afterEach, expect } from 'vitest';
 import { server } from './__mocks__/msw/server';
-
-declare global {
-  namespace Vi {
-    interface JestAssertion<T = unknown> extends jest.Matchers<void, T>, TestingLibraryMatchers<T, void> {}
-  }
-}
 
 expect.extend(matchers);
 
