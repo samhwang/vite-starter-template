593e747b70e61769d28f82a2c509996bf4e53852
Sam Huynh <sam.huynh@lyka.com.au>
2025-01-13 11:34:11 +1100

    feat: install and configure react query

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,18 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from '@tanstack/react-router';
+import { useState } from 'react';
 import { appRouter } from './router';
 
 function App() {
-  return <RouterProvider router={appRouter} />;
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={appRouter} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
