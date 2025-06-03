1256ae939349c917ffeca257fb03c929cee6825c
Sam Huynh <sam.huynh@lyka.com.au>
2025-06-03 21:43:44 +1000

    feat: update router devtools import

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,13 +1,5 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router';
-import { lazy } from 'react';
-
-const TanStackRouterDevtools = import.meta.env.PROD
-  ? () => null
-  : lazy(() =>
-      import('@tanstack/router-devtools').then((res) => ({
-        default: res.TanStackRouterDevtools,
-      }))
-    );
+import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 export const Route = createRootRoute({
   component: () => (
